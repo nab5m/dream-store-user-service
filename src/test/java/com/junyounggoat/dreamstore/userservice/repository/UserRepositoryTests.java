e929634df6f4310d9e22e552869c113792147dd3
JunYoung Kim <sk990420@naver.com>
2023-11-02 13:02:49 +0900

    Add findUserByNaverId, insertNaverUser tests to UserRepositoryTests.

--- src/test/java/com/junyounggoat/dreamstore/userservice/repository/UserRepositoryTests.java
+++ src/test/java/com/junyounggoat/dreamstore/userservice/repository/UserRepositoryTests.java
@@ -1,10 +1,15 @@
 package com.junyounggoat.dreamstore.userservice.repository;
 
+import static com.junyounggoat.dreamstore.userservice.entity.NaverUserTests.createTestNaverUser;
+import static com.junyounggoat.dreamstore.userservice.entity.UserLoginCategoryTests.createTestUserLoginCategory;
 import static com.junyounggoat.dreamstore.userservice.entity.UserPrivacyUsagePeriodTests.createTestUserPrivacyUsagePeriod;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import com.junyounggoat.dreamstore.userservice.constant.UserLoginCategoryCode;
 import com.junyounggoat.dreamstore.userservice.constant.UserPrivacyUsagePeriodCode;
+import com.junyounggoat.dreamstore.userservice.entity.NaverUser;
 import com.junyounggoat.dreamstore.userservice.entity.User;
+import com.junyounggoat.dreamstore.userservice.entity.UserLoginCategory;
 import com.junyounggoat.dreamstore.userservice.entity.UserPrivacyUsagePeriod;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -65,5 +70,47 @@ public class UserRepositoryTests {
 
         // then
         assertEquals(userIdList, List.of(privacyExpiredAndNeedsRemove.getUser().getUserId()));
+    }
+
+    @Test
+    @DisplayName("findUserByNaverId: User 반환 성공")
+    public void findUserByNaverIdReturnUser() {
+        // given
+        NaverUser testNaverUser = createTestNaverUser(entityManager);
+
+        // when
+        User foundNaverUser = userRepository.findUserByNaverId(testNaverUser.getNaverId());
+
+        // then
+        assertEquals(testNaverUser.getUserLoginCategory().getUser().getUserId(), foundNaverUser.getUserId());
+    }
+    
+    @Test
+    @DisplayName("findUserByNaverId: 존재하지 않는 사용자로 null 반환")
+    public void findUserByNaverIdReturnNull() {
+        // given
+
+        // when
+        User foundNaverUser = userRepository.findUserByNaverId("");
+
+        // then
+        assertNull(foundNaverUser);
+    }
+
+    @Test
+    @DisplayName("insertNaverUser 정상 작동")
+    public void insertNaverUserSuccess() {
+        // given
+        UserLoginCategory testUserLoginCategory = createTestUserLoginCategory(entityManager, UserLoginCategoryCode.naverUser);
+
+        // when
+        NaverUser testNaverUser = NaverUser.builder()
+                .userLoginCategory(testUserLoginCategory)
+                .naverId("testNaverUser")
+                .build();
+        NaverUser createdNaverUser = userRepository.insertNaverUser(testNaverUser);
+
+        // then
+        assertTrue(createdNaverUser.getNaverUserId() > 0);
     }
 }
