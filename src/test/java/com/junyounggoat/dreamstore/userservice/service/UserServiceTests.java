3f234644cc05573cfc45942dc5296fede14f08e7
JunYoung Kim <sk990420@naver.com>
2023-11-02 14:32:34 +0900

    Add EmbeddedRedisConfig to UserServiceTests and fix mock error

--- src/test/java/com/junyounggoat/dreamstore/userservice/service/UserServiceTests.java
+++ src/test/java/com/junyounggoat/dreamstore/userservice/service/UserServiceTests.java
@@ -1,16 +1,18 @@
 package com.junyounggoat.dreamstore.userservice.service;
 
+import com.junyounggoat.dreamstore.userservice.config.EmbeddedRedisConfig;
+import com.junyounggoat.dreamstore.userservice.dto.CreateNaverUserRequestDTO;
 import com.junyounggoat.dreamstore.userservice.dto.CreateUserDTO;
 import com.junyounggoat.dreamstore.userservice.dto.TokenResponseDTO;
 import com.junyounggoat.dreamstore.userservice.entity.NaverUser;
 import com.junyounggoat.dreamstore.userservice.entity.UserLoginCategory;
 import com.junyounggoat.dreamstore.userservice.validation.BadRequestException;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -25,12 +27,27 @@ import static org.mockito.Mockito.doThrow;
 
 @SpringBootTest
 @Transactional
+@Import({ EmbeddedRedisConfig.class })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTests {
+    @Autowired
+    private EmbeddedRedisConfig embeddedRedisConfig;
+
     @Autowired
     private UserService userService;
 
     @SpyBean
     private NaverLoginService naverLoginService;
+
+    @BeforeAll
+    public void startRedisServer() {
+        embeddedRedisConfig.startRedis();
+    }
+
+    @AfterAll
+    public void stopRedisServer() {
+        embeddedRedisConfig.stopRedis();
+    }
 
     private TokenResponseDTO createNaverUser() {
         CreateNaverUserRequestDTO createNaverUserRequestDTO = CreateNaverUserRequestDTO.builder()
@@ -67,7 +84,7 @@ public class UserServiceTests {
     public void createNaverUserFailsWhenRequestNaverUserFails() {
         // given
         doThrow(new BadRequestException(REQUEST_NAVER_USER_PROFILE_FAILED))
-                .when(naverLoginService.createNaverUser(any(UserLoginCategory.class), anyString()));
+                .when(naverLoginService).createNaverUser(any(UserLoginCategory.class), anyString());
 
         // when
         Executable createNaverUserExecutable = this::createNaverUser;
