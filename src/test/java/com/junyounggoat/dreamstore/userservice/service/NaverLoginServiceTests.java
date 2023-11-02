d640a4ae26ae9ba75f213143f6b2a5d45d315657
JunYoung Kim <sk990420@naver.com>
2023-11-02 13:26:48 +0900

    Add createNaverUser tests to NaverLoginServiceTests.

--- src/test/java/com/junyounggoat/dreamstore/userservice/service/NaverLoginServiceTests.java
+++ src/test/java/com/junyounggoat/dreamstore/userservice/service/NaverLoginServiceTests.java
@@ -1,14 +1,17 @@
 package com.junyounggoat.dreamstore.userservice.service;
 
 import com.junyounggoat.dreamstore.userservice.config.EmbeddedRedisConfig;
+import com.junyounggoat.dreamstore.userservice.constant.UserLoginCategoryCode;
 import com.junyounggoat.dreamstore.userservice.dto.NaverUserProfileResponseDTO;
 import com.junyounggoat.dreamstore.userservice.dto.TokenResponseDTO;
 import com.junyounggoat.dreamstore.userservice.entity.NaverUser;
+import com.junyounggoat.dreamstore.userservice.entity.UserLoginCategory;
 import com.junyounggoat.dreamstore.userservice.validation.BadRequestException;
 import com.junyounggoat.dreamstore.userservice.validation.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -16,10 +19,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.junyounggoat.dreamstore.userservice.entity.NaverUserTests.createTestNaverUser;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static com.junyounggoat.dreamstore.userservice.entity.UserLoginCategoryTests.createTestUserLoginCategory;
+import static com.junyounggoat.dreamstore.userservice.service.NaverLoginService.REQUEST_NAVER_USER_PROFILE_FAILED;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 @SpringBootTest
 @Import({ EmbeddedRedisConfig.class })
@@ -77,5 +82,39 @@ public class NaverLoginServiceTests {
         TokenResponseDTO tokenResponseDTO = naverLoginService.loginNaverUser("");
         assertEquals(TokenService.getUserIdFromAccessToken(tokenResponseDTO.getAccessToken()),
                 naverUser.getUserLoginCategory().getUser().getUserId());
+    }
+
+    @Test
+    @DisplayName("네이버사용자 생성: 성공")
+    public void createNaverUserSuccess() {
+        // given
+        UserLoginCategory testUserLoginCategory = createTestUserLoginCategory(entityManager, UserLoginCategoryCode.naverUser);
+
+        doReturn(NaverUserProfileResponseDTO.builder()
+                .response(NaverUserProfileResponseDTO.ResponseDTO.builder()
+                        .id("testNaverUser")
+                        .build())
+                .build())
+                .when(naverLoginService).requestNaverUserProfile(anyString());
+
+        // when
+        NaverUser testNaverUser = naverLoginService.createNaverUser(testUserLoginCategory, "");
+
+        // then
+        assertTrue(testNaverUser.getNaverUserId() > 0);
+    }
+
+    @Test
+    @DisplayName("네이버사용자 생성 : 네이버사용자 프로필 조회 실패")
+    public void createNaverUserFailsWhenRequestNaverUserProfileFails() {
+        // given
+        doThrow(new BadRequestException(REQUEST_NAVER_USER_PROFILE_FAILED))
+                .when(naverLoginService).requestNaverUserProfile(anyString());
+
+        // when
+        Executable createNaverUserExecutable = () -> naverLoginService.createNaverUser(null, "");
+
+        // then
+        assertThrows(BadRequestException.class, createNaverUserExecutable);
     }
 }
