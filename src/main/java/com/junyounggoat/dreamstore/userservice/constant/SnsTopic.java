5108180feab5724ddb64e8ada05c96146bde631b
김준영 <junyoung.kim@ecomap.kr>
2024-02-25 13:08:20 +0900

    Fix: SnsTopic.ExpireUserPrivacy arn 및 이름 수정

--- src/main/java/com/junyounggoat/dreamstore/userservice/constant/SnsTopic.java
+++ src/main/java/com/junyounggoat/dreamstore/userservice/constant/SnsTopic.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum SnsTopic {
-    ExpireUserPrivacy("arn:aws:sns:ap-northeast-2:269669414396:ExpireUserPrivacy", "ExpireUserPrivacy");
+    ExpireUserPrivacy("arn:aws:sns:ap-northeast-2:856155157154:ExpireUserPrivacy.fifo", "ExpireUserPrivacy.fifo");
 
     private final String arn;
     private final String name;
